8205a25e6098a235162b4fe10e201abe05b42eb9
이승태 <bs007bsk@naver.com>
2023-01-31 17:58:51 +0900

    20230131

--- start/start.py
+++ start/start.py
@@ -1119,27 +1119,27 @@
 
 
 
-class Person():
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
+# class Person():
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
 
-    @classmethod
-    def get_age(cls,name,year):
-        cls.name=name
-        cls.age=2023-year
-        return cls
+#     @classmethod
+#     def get_age(cls,name,year):
+#         cls.name=name
+#         cls.age=2023-year
+#         return cls
     
-    def check_age(cls):
-        return cls.age>=19
+#     def check_age(cls):
+#         return cls.age>=19
    
     
-person1=Person('Mark',20)
-person2=Person.get_age('Rohan',1992)
+# person1=Person('Mark',20)
+# person2=Person.get_age('Rohan',1992)
 
-print(person1.name, person1.age) 
-print(person2.name, person2.age)
-print(person1.check_age())
-print(person2.check_age())
+# print(person1.name, person1.age) 
+# print(person2.name, person2.age)
+# print(person1.check_age())
+# print(person2.check_age())
 
 
